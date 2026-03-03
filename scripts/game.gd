f60afd6c917086c5dbab873746df67351b9bfba4
Dead-Vortex <milesohalpin@gmail.com>
2026-03-03 14:50:55 -0800

    turn actions
    - all buttons in the ui now function (buy/eat sad meal, eat drug, roll die)
    - camera panning no longer switches active ui button (rebinded ui_dir)
    - really cool modular die rolling and space moving system (epic 1337)

--- scripts/game.gd
+++ scripts/game.gd
@@ -8,6 +8,7 @@ extends Node2D
 @onready var player3 = $Cards
 @onready var player4 = $Milk
 var player_piece_names : Dictionary = {1: "Goat", 2: "Bag", 3: "Cards", 4: "Milk"}
+var current_player : int = 1
 
 # UI
 @onready var status_label = $CanvasLayer/PanelContainer/VBoxContainer/StatusLabel
@@ -15,12 +16,15 @@ var player_piece_names : Dictionary = {1: "Goat", 2: "Bag", 3: "Cards", 4: "Milk
 @onready var purchase_sad_meal_button = $CanvasLayer/PanelContainer/VBoxContainer/BuySadMeal
 @onready var eat_sad_meal_button = $CanvasLayer/PanelContainer/VBoxContainer/SadMeal
 @onready var rhymes_with_grug_button = $CanvasLayer/PanelContainer/VBoxContainer/Drug
+var before_turn : bool = true
+signal die_rolled
+var die_roll : int = 2
 
 var player_stats : Array = [
-	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0},
-	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0},
-	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0},
-	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0}
+	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0, "space": 0},
+	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0, "space": 0},
+	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0, "space": 0},
+	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0, "space": 0}
 ]
 
 # Board Spaces
@@ -38,9 +42,10 @@ func _ready() -> void:
 	game_turn(1)
 
 func _process(delta: float) -> void:
-	camera.offset += Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 1000 * delta
+	camera.offset += Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down") * 1000 * delta
 
 func game_turn(player):
+	current_player = player
 	status_label.text = player_piece_names[player] + " - $" + str(player_stats[player - 1]["money"]) + "\nHunger: " + str(player_stats[player - 1]["hunger"]) + "/10\nInebriation: " + str(player_stats[player - 1]["inebriation"]) + "/10"
 	die_button.text = "Roll Die"
 	die_button.disabled = false
@@ -58,7 +63,72 @@ func game_turn(player):
 		rhymes_with_grug_button.disabled = true
 	else:
 		rhymes_with_grug_button.disabled = false
+	await(die_rolled)
+	die_button.disabled = true
+	purchase_sad_meal_button.disabled = true
+	eat_sad_meal_button.disabled = true
+	rhymes_with_grug_button.disabled = true
+	for i in 10:
+		if player_stats[player - 1]["inebriation"] >= 4:
+			die_roll = (randi_range(1, 6) + randi_range(1, 6))
+		else:
+			die_roll = randi_range(1, 6)
+		die_button.text = str(die_roll)
+		await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.5).timeout
+	move_player_spaces(player, die_roll)
 
+func update_ui():
+	status_label.text = player_piece_names[current_player] + " - $" + str(player_stats[current_player - 1]["money"]) + "\nHunger: " + str(player_stats[current_player - 1]["hunger"]) + "/10\nInebriation: " + str(player_stats[current_player - 1]["inebriation"]) + "/10"
+	if player_stats[current_player - 1]["money"] < 5:
+		purchase_sad_meal_button.disabled = true
+	else:
+		purchase_sad_meal_button.disabled = false
+	eat_sad_meal_button.text = "Consume a Sad Meal™️ (" + str(player_stats[current_player - 1]["sadmeals"]) + ")"
+	if player_stats[current_player - 1]["sadmeals"] <= 0:
+		eat_sad_meal_button.disabled = true
+	else:
+		eat_sad_meal_button.disabled = false
+	rhymes_with_grug_button.text = "Consume a Drug (" + str(player_stats[current_player - 1]["drugs"]) + ")"
+	if player_stats[current_player - 1]["drugs"] <= 0:
+		rhymes_with_grug_button.disabled = true
+	else:
+		rhymes_with_grug_button.disabled = false
+
+func move_player_spaces(player : int, spaces : int):
+	for i in spaces:
+		player_stats[player - 1]["space"] += 1
+		get_node(player_piece_names[player]).global_position = get_node("BoardSpaces/Space" + str(player_stats[player - 1]["space"])).global_position
+		await get_tree().create_timer(0.5).timeout
+
+func _on_dice_roll_button_pressed() -> void:
+	emit_signal("die_rolled")
+
+func _on_buy_sad_meal_button_pressed() -> void:
+	if before_turn and player_stats[current_player - 1]["money"] >= 5:
+		player_stats[current_player - 1]["money"] -= 5
+		player_stats[current_player - 1]["sadmeals"] += 1
+		update_ui()
+
+func _on_eat_sad_meal_pressed() -> void:
+	if before_turn and player_stats[current_player - 1]["sadmeals"] > 0:
+		player_stats[current_player - 1]["sadmeals"] -= 1
+		if player_stats[current_player - 1]["inebriation"] >= 4:
+			player_stats[current_player - 1]["hunger"] -= 5
+		else:
+			player_stats[current_player - 1]["hunger"] -= 2
+		if player_stats[current_player - 1]["hunger"] < 0:
+			player_stats[current_player - 1]["hunger"] = 0
+	update_ui()
+
+func _on_use_drug_pressed() -> void:
+	if before_turn and player_stats[current_player - 1]["drugs"] > 0:
+		player_stats[current_player - 1]["drugs"] -= 1
+		player_stats[current_player - 1]["inebriation"] += 3
+		if player_stats[current_player - 1]["inebriation"] > 10:
+			# eliminate player
+			pass
+	update_ui()
 
 
 
