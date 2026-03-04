89651fcc52fb99f18c308be07c2910fe31bf986a
Dead-Vortex <milesohalpin@gmail.com>
2026-03-03 17:39:53 -0800

    space detection (among other things)
    - space detection (currently just prints to console)
    - fixed bug where looping back to start would crash the game
    - game turns now loop (12341234...)
    - added camera bounds + slight smoothing
    - shortened turn code (ui updates at start of turn now use update_ui())
    - when you are inebriated the die roll button is now correctly plural
    - added snarky comments everywhere for "organization" (because i was bored)

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,7 +6,7 @@ extends Node2D
 @onready var player1 = $Goat
 @onready var player2 = $Bag
 @onready var player3 = $Cards
-@onready var player4 = $Milk
+@onready var player4 = $Milk # these might be useless? if they are i will remove them (this is a reminder)
 var player_piece_names : Dictionary = {1: "Goat", 2: "Bag", 3: "Cards", 4: "Milk"}
 var current_player : int = 1
 
@@ -15,13 +15,15 @@ var current_player : int = 1
 @onready var die_button = $CanvasLayer/PanelContainer/VBoxContainer/Dice
 @onready var purchase_sad_meal_button = $CanvasLayer/PanelContainer/VBoxContainer/BuySadMeal
 @onready var eat_sad_meal_button = $CanvasLayer/PanelContainer/VBoxContainer/SadMeal
-@onready var rhymes_with_grug_button = $CanvasLayer/PanelContainer/VBoxContainer/Drug
+@onready var rhymes_with_grug_button = $CanvasLayer/PanelContainer/VBoxContainer/Drug # I have an idea!
+
+# Turn logic
 var before_turn : bool = true
 signal die_rolled
 var die_roll : int = 2
 
-var player_stats : Array = [
-	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0, "space": 0},
+var player_stats : Array = [ # Self explanitory.
+	{"money": 10, "hunger": 0, "inebriation": 5, "sadmeals": 1, "drugs": 0, "space": 0},
 	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0, "space": 0},
 	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0, "space": 0},
 	{"money": 10, "hunger": 0, "inebriation": 0, "sadmeals": 1, "drugs": 0, "space": 0}
@@ -39,30 +41,32 @@ var npc_spaces : Array = [5, 11, 15, 18, 22, 24]
 # Bar - 21
 
 func _ready() -> void:
-	game_turn(1)
+	while true:
+		await(game_turn(1))
+		await(game_turn(2))
+		await(game_turn(3))
+		await(game_turn(4))
 
 func _process(delta: float) -> void:
-	camera.offset += Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down") * 1000 * delta
+	# Camera controls
+	camera.global_position.x += Input.get_axis("camera_left", "camera_right") * 1000 * delta
+	camera.global_position.y += Input.get_axis("camera_up", "camera_down") * 1000 * delta
+	# I feel like there's a better way to do this but i dont know how so have unoptimized camera bounds
+	if camera.global_position.x < -232:
+		camera.global_position.x = -232
+	elif camera.global_position.x > 232:
+		camera.global_position.x = 232
+	if camera.global_position.y > 568:
+		camera.global_position.y = 568
+	elif camera.global_position.y < -568:
+		camera.global_position.y = -568
 
 func game_turn(player):
 	current_player = player
-	status_label.text = player_piece_names[player] + " - $" + str(player_stats[player - 1]["money"]) + "\nHunger: " + str(player_stats[player - 1]["hunger"]) + "/10\nInebriation: " + str(player_stats[player - 1]["inebriation"]) + "/10"
-	die_button.text = "Roll Die"
+	# Initialize UI
 	die_button.disabled = false
-	if player_stats[player - 1]["money"] < 5:
-		purchase_sad_meal_button.disabled = true
-	else:
-		purchase_sad_meal_button.disabled = false
-	eat_sad_meal_button.text = "Consume a Sad Meal™️ (" + str(player_stats[player - 1]["sadmeals"]) + ")"
-	if player_stats[player - 1]["sadmeals"] <= 0:
-		eat_sad_meal_button.disabled = true
-	else:
-		eat_sad_meal_button.disabled = false
-	rhymes_with_grug_button.text = "Consume a Drug (" + str(player_stats[player - 1]["drugs"]) + ")"
-	if player_stats[player - 1]["drugs"] <= 0:
-		rhymes_with_grug_button.disabled = true
-	else:
-		rhymes_with_grug_button.disabled = false
+	update_ui()
+	# Die Roll
 	await(die_rolled)
 	die_button.disabled = true
 	purchase_sad_meal_button.disabled = true
@@ -76,10 +80,30 @@ func game_turn(player):
 		die_button.text = str(die_roll)
 		await get_tree().create_timer(0.2).timeout
 	await get_tree().create_timer(0.5).timeout
-	move_player_spaces(player, die_roll)
+	await(move_player_spaces(player, die_roll))
+	# Board Space Actions
+	if police_inspection_spaces.has(player_stats[player - 1]["space"]):
+		print(player_piece_names[player] + " landed on Police Inspection")
+	elif dark_alleyway_spaces.has(player_stats[player - 1]["space"]):
+		print(player_piece_names[player] + " landed on Dark Alleyway")
+	elif heist_spaces.has(player_stats[player - 1]["space"]):
+		print(player_piece_names[player] + " landed on Heist")
+	elif npc_spaces.has(player_stats[player - 1]["space"]):
+		print(player_piece_names[player] + " landed on NPC")
+	elif player_stats[player - 1]["space"] == 2:
+		print(player_piece_names[player] + " landed on Unemployment Tax")
+	elif player_stats[player - 1]["space"] == 7:
+		print(player_piece_names[player] + " landed on Casino")
+	elif player_stats[player - 1]["space"] == 21:
+		print(player_piece_names[player] + " landed on Bar")
 
 func update_ui():
 	status_label.text = player_piece_names[current_player] + " - $" + str(player_stats[current_player - 1]["money"]) + "\nHunger: " + str(player_stats[current_player - 1]["hunger"]) + "/10\nInebriation: " + str(player_stats[current_player - 1]["inebriation"]) + "/10"
+	# haha funny small text difference
+	if player_stats[current_player - 1]["inebriation"] >= 4:
+		die_button.text = "Roll Dice"
+	else:
+		die_button.text = "Roll Die"
 	if player_stats[current_player - 1]["money"] < 5:
 		purchase_sad_meal_button.disabled = true
 	else:
@@ -98,6 +122,8 @@ func update_ui():
 func move_player_spaces(player : int, spaces : int):
 	for i in spaces:
 		player_stats[player - 1]["space"] += 1
+		if player_stats[player - 1]["space"] > 27 and player_stats[player - 1]["space"] != 99:
+			player_stats[player - 1]["space"] = 0
 		get_node(player_piece_names[player]).global_position = get_node("BoardSpaces/Space" + str(player_stats[player - 1]["space"])).global_position
 		await get_tree().create_timer(0.5).timeout
 
@@ -113,13 +139,13 @@ func _on_buy_sad_meal_button_pressed() -> void:
 func _on_eat_sad_meal_pressed() -> void:
 	if before_turn and player_stats[current_player - 1]["sadmeals"] > 0:
 		player_stats[current_player - 1]["sadmeals"] -= 1
-		if player_stats[current_player - 1]["inebriation"] >= 4:
+		if player_stats[current_player - 1]["inebriation"] >= 4: # Inebriation Bonus
 			player_stats[current_player - 1]["hunger"] -= 5
 		else:
 			player_stats[current_player - 1]["hunger"] -= 2
 		if player_stats[current_player - 1]["hunger"] < 0:
 			player_stats[current_player - 1]["hunger"] = 0
-	update_ui()
+		update_ui()
 
 func _on_use_drug_pressed() -> void:
 	if before_turn and player_stats[current_player - 1]["drugs"] > 0:
@@ -128,10 +154,10 @@ func _on_use_drug_pressed() -> void:
 		if player_stats[current_player - 1]["inebriation"] > 10:
 			# eliminate player
 			pass
-	update_ui()
+		update_ui()
 
 
-
+# Old, bad code because i forgot dictionaries exist
 ## Player 1
 #var player1_money : int = 10
 #var player1_hunger : int = 0
